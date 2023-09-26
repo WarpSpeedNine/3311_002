416f0f58eaed678e35364cea2a64170fadfc47aa
Heath Christman <hcc7591@mavs.uta.edu>
2023-09-25 20:37:52 -0500

    Initial Home Screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+
 
 import React from 'react';
+import { Image } from 'react-native';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -68,30 +64,18 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+     <ScrollView
+  contentInsetAdjustmentBehavior="automatic"
+  style={backgroundStyle}>
+  <Image source={require('./images/logo.png')} style={{width: 410, height: 410}} />
+  <View
+    style={{
+      backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    }}>
+    <Section title="Welcome to a new workout!!">
+    </Section>
+  </View>
+</ScrollView>
     </SafeAreaView>
   );
 }
