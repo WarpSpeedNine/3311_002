23fce6247c0ecdc38d4a96ef8780a6662ac6db4f
Heath Christman <hcc7591@mavs.uta.edu>
2023-10-07 19:33:29 -0500

    basic home screen with button attempt

---
+++ pages/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default HomeScreen;
