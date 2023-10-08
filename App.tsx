90c113148c1cafda987e5312913f8ec79f666490
Heath Christman <hcc7591@mavs.uta.edu>
2023-10-07 21:44:21 -0500

    Initial RD Home Screen loads

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './pages/HomeScreen';
 import DetailsScreen from './pages/DetailScreen';
 
--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
-            <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+            <Text style={styles.header}>Fitness Tracker</Text>
+
+            <TouchableOpacity style={styles.button} onPress={() => {/*Handle Button Press*/}}>
+                <Text style={styles.buttonText}>History</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => {/* Handle "Manual" button press */}}>
+                <Text style={styles.buttonText}>Manual</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => {/* Handle "Routines" button press */}}>
+                <Text style={styles.buttonText}>Routines</Text>
+            </TouchableOpacity>
+
+                <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+            
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
+      backgroundColor: 'black',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
-});
+    header: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+      marginBottom: 50,  // Adds some space below the header
+    },
+    button: {
+      backgroundColor: 'gray',  // Basic button color. You can choose anything.
+      padding: 10,
+      marginVertical: 10,  // Adds space between buttons
+      width: 200,  // Fixed button width
+      alignItems: 'center',  // Center the text inside the button
+      borderRadius: 8,  // Rounded corners
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 18,
+    },
+  });
 
 export default HomeScreen;
