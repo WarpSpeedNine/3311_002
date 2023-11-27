3487f11e96af77caf88aee902a7776b6ed419c14
Heath Christman <hcc7591@mavs.uta.edu>
2023-11-27 16:57:56 -0600

    Feilds and Add Set populate and retain data - add monikers for each field

--- pages/LogWorkoutScreen.js
+++ pages/LogWorkoutScreen.js
@@ -29,9 +29,37 @@ const LogWorkoutScreen = ({route, navigation}) => {
   }, [route.params?.selectedExercise]);
 
   // Function to add a new set to the current exercise
-  const addSet = () => {
-    const newSet = {setId: currentSets.length + 1, weight: '', reps: ''};
-    setCurrentSets([...currentSets, newSet]);
+  const addSet = exerciseIndex => {
+    setExercises(prevExercises => {
+      return prevExercises.map((exercise, index) => {
+        if (index === exerciseIndex) {
+          const newSet = {
+            setId: exercise.sets.length + 1,
+            weight: '',
+            reps: '',
+          };
+          return {...exercise, sets: [...exercise.sets, newSet]};
+        }
+        return exercise;
+      });
+    });
+  };
+  const handleSetChange = (newValue, exerciseIndex, setIndex, field) => {
+    setExercises(prevExercises => {
+      const updatedExercises = prevExercises.map((exercise, index) => {
+        if (index === exerciseIndex) {
+          const updatedSets = exercise.sets.map((set, sIndex) => {
+            if (sIndex === setIndex) {
+              return {...set, [field]: newValue};
+            }
+            return set;
+          });
+          return {...exercise, sets: updatedSets};
+        }
+        return exercise;
+      });
+      return updatedExercises;
+    });
   };
 
   return (
@@ -39,7 +67,30 @@ const LogWorkoutScreen = ({route, navigation}) => {
       {exercises.map((exercise, index) => (
         <View key={index}>
           <Text style={styles.exerciseText}>{exercise.name}</Text>
-          {/* Here you can also map over exercise.sets to display set details */}
+          {exercise.sets.map((set, setIndex) => (
+            <View key={setIndex} style={styles.setContainer}>
+              <Text>Set {set.setId}: </Text>
+              <TextInput
+                placeholder="Weight"
+                value={set.weight}
+                onChangeText={text =>
+                  handleSetChange(text, index, setIndex, 'weight')
+                }
+                style={styles.input}
+              />
+              <TextInput
+                placeholder="Reps"
+                value={set.reps}
+                onChangeText={text =>
+                  handleSetChange(text, index, setIndex, 'reps')
+                }
+                style={styles.input}
+              />
+            </View>
+          ))}
+          <TouchableOpacity style={styles.button} onPress={() => addSet(index)}>
+            <Text style={styles.buttonText}>Add Set</Text>
+          </TouchableOpacity>
         </View>
       ))}
       <TouchableOpacity
@@ -85,6 +136,20 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 15, // Some space between exercises
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#FFA500',
+    color: '#FFFFFF', // White color for the text
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    margin: 5,
+    minWidth: 50, // Minimum width for the input fields
+  },
+  setContainer: {
+    flexDirection: 'row', // Aligns the set details horizontally
+    alignItems: 'center', // Centers items vertically within the row
+    marginVertical: 5, // Adds vertical margin for each set row
   },
 });
 
